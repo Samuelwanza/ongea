4c6afb113d56dce958f26aad2cabf509507576ed
Samuel wanza <s.wanza@alustudent.com>
2023-12-01 19:50:04 +0200

    sorted rubocop issue

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 RSpec.describe Comment, type: :model do
-  subject { Comment.new(user_id: 1, post_id: 1, text: "We shall overcome") }
+  subject { Comment.new(user_id: 1, post_id: 1, text: 'We shall overcome') }
   before { subject.save }
   it 'update_comments_counter should  function' do
     expect { subject.send(:update_comments_counter) }
