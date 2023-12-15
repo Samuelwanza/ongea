ac3cfb8da11580f769afa44cf62668f9e4b2bf92
Samuel wanza <s.wanza@alustudent.com>
2023-12-15 20:40:46 +0200

    rectified code

--- spec/views/post/post_index_spec.rb
+++ spec/views/post/post_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User post index page', type: :system do
+RSpec.describe 'User post index page as well pagination', type: :system do
   before(:each) do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     @post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
